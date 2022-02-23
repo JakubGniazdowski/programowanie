b873f671cef809594209f9c5b3df28c4f201727f
JakubGniazdowski <98549776+JakubGniazdowski@users.noreply.github.com>
2022-02-23 14:55:35 +0100

    initial name

--- nauka klas/otherfile.py
+++ nauka klas/otherfile.py
@@ -8,6 +8,23 @@ manynames.f()
 manynames.g()
 print(manynames.C.x)
 I=manynames.C()
-print(I.x)
+print(I.x) # tu pobieramy zmienną z klasy
 I.m()
-print(I.x)
+print(I.x) # a tu pobieramy zmienną z instancji, własna zmienna X z instancji jest tworzona dopiero gdy wywołamy metodę
+
+x=11
+
+def g1():
+    print(x)
+def g2():
+    global x
+    x=22
+def h1():
+    x=33
+    def nested():
+        print(x)
+def h2():
+    x=33
+    def nested():
+        nonlocal x
+        x=44
